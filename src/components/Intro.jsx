8df5e4daddaf2074e6983d0037bdaa09294c0ab1
Mayank Kumar <96490105+mank-423@users.noreply.github.com>
2023-03-30 00:12:59 +0530

    Responsiveness of images

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -27,14 +27,14 @@ export default function Intro(){
             <div className="left grid grid-cols-3 gap-3 m-8">
                 
                 <div></div>
-                <div><img src={cpp} alt="Cpp" className="lg:h-20 lg:w-20 md:h-12 md:h-12 rounded-full opacity-50 rotate-12 "></img></div>
-                <div><img src={py} alt="py" className="lg:h-20 lg:w-28 rounded-full opacity-50 rotate-12 "></img></div>
-                <div><img src={ic} alt="ic" className="lg:h-12 lg:w-20 rounded-sm m-2 opacity-50 rotate-12 "></img></div>
-                <div><img src={c} alt="ic" className="lg:h-20 rounded-sm m-2 opacity-50"></img></div>
-                <div><img src={eth} alt="eth" className="lg:h-20 lg:w-20 rounded-full opacity-50 rotate-12 "></img></div>
-                <div><img src={sol} alt="sol" className="lg:h-20 lg:w-20 rounded-full opacity-50 rotate-12"></img> </div>
-                <div><img src={android} alt="android" className="lg:h-24 lg:w-28 rounded-full opacity-70 rotate-12"></img> </div>
-                <div><img src={motoko} alt="motoko" className="lg:h-24 lg:w-28 md:h-16 rounded-full opacity-50 rotate-12"></img> </div>
+                <div><img src={cpp} alt="Cpp" className="h-8 w-20 lg:h-20 lg:w-20 md:w-12 md:h-12  rounded-full opacity-0 lg:opacity-50 lg:rotate-12 "></img></div>
+                <div><img src={py} alt="py" className="h-8 w-20 lg:h-20 lg:w-28 rounded-full opacity-0 lg:opacity-50 lg:rotate-12 "></img></div>
+                <div><img src={ic} alt="ic" className="h-8 w-20 lg:h-12 lg:w-20 hidden rounded-sm m-2 opacity-0 lg:opacity-50 lg:rotate-12 "></img></div>
+                <div><img src={c} alt="c" className="h-8 w-24 lg:h-20 rounded-sm m-2 opacity-0 lg:opacity-50"></img></div>
+                <div><img src={eth} alt="eth" className="h-12 w-20 lg:h-20 lg:w-20 rounded-full opacity-0 lg:opacity-50 lg:rotate-12 "></img></div>
+                <div><img src={sol} alt="sol" className="h-4 w-20 lg:h-20 lg:w-20 rounded-full opacity-0 lg:opacity-50 lg:rotate-12"></img> </div>
+                <div><img src={android} alt="android" className="h-12 w-24 lg:h-24 lg:w-28 rounded-full opacity-0 lg:opacity-70 lg:rotate-12"></img> </div>
+                <div><img src={motoko} alt="motoko" className="h-12 w-20 lg:h-24 lg:w-28 md:h-16 rounded-full opacity-0 lg:opacity-50 lg:rotate-12"></img> </div>
             </div>
 
             <div>
@@ -42,21 +42,20 @@ export default function Intro(){
                     <img src={me} alt="My pic"></img>
                     <br></br>
                     <br></br>
-                    {/*Full Stack | Python | Web3.0*/}
-                    <p className="text-2xl font-bold">Hellooo!</p>
+                    <p className="text-2xl font-bold">Full Stack | Python | C++</p>
                 </center>
             </div>
 
             <div className="right grid grid-cols-3 gap-3 m-8">
                 <div></div>
-                <div><img src={html} alt="HTML" className="lg:h-32 lg:w-36 md:h-20 md:w-24  rounded-full opacity-50 rotate-12 "></img></div>
-                <div><img src={bootstrap} alt="HTML" className="lg:h-16 lg:w-20 md:h-20 md:w-24 rounded-full opacity-50 rotate-12 "></img></div>
-                <div><img src={css} alt="Css" className="lg:h-24 lg:w-44  rounded-full opacity-50 rotate-12 "></img></div>
-                <div><img src={mongoose} alt="mongoose" className="lg:h-32 lg:w-64 opacity-80"></img></div>
-                <div><img src={js} alt="Js" className="lg:h-24 lg:w-24 rounded-full opacity-60 rotate-45 "></img></div>
-                <div><img src={mongo} alt="mongo" className="lg:h-28 lg:w-60 opacity-80"></img></div>
-                <div><img src={node} alt="node" className="lg:h-28 lg:w-40 rounded-full opacity-60 -rotate-12"></img></div>
-                <div><img src={react} alt="react" className="lg:h-28 lg:w-60 rounded-full opacity-80"></img></div>
+                <div><img src={html} alt="HTML" className="h-12 w-20 lg:h-32 lg:w-36 md:h-20 md:w-24  rounded-full opacity-0 lg:opacity-50 lg:rotate-12 "></img></div>
+                <div><img src={bootstrap} alt="HTML" className="h-12 w-20 lg:h-16 lg:w-20 md:h-20 md:w-24 rounded-full opacity-0 lg:opacity-50 lg:rotate-12 "></img></div>
+                <div><img src={css} alt="Css" className="h-12 w-20 lg:h-24 lg:w-44  rounded-full opacity-0 lg:opacity-50 lg:rotate-12 "></img></div>
+                <div><img src={mongoose} alt="mongoose" className="h-12 w-20 lg:h-32 lg:w-64 opacity-0 lg:opacity-80"></img></div>
+                <div><img src={js} alt="Js" className="h-12 w-20 lg:h-24 lg:w-24 rounded-full opacity-0 lg:opacity-60 lg:rotate-45 "></img></div>
+                <div><img src={mongo} alt="mongo" className="h-12 w-20 lg:h-28 lg:w-60 opacity-0 lg:opacity-80"></img></div>
+                <div><img src={node} alt="node" className="h-12 w-20 lg:h-28 lg:w-40 rounded-full opacity-0 lg:opacity-60 lg:rotate-12"></img></div>
+                <div><img src={react} alt="react" className="h-12 w-20 lg:h-28 lg:w-60 rounded-full opacity-0 lg:opacity-80"></img></div>
             </div>
         </div>
             
